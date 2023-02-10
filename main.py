7b4ae0b9134b8a545cd56867381ce804bbb820f2
DanilkaMoeImya <124866507+DanilkaMoeImya@users.noreply.github.com>
2023-02-10 22:33:13 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
-
+import random
 from modeel import *
+from slovar import *
+
 
 
 
@@ -11,32 +13,33 @@ def init_v():
         db.create_tables([CLIENTS,ORDERS])
         a="Table created"
     return a
+
     
 def fill_v():
     with db:
-        clients = [{'name':'Ivan','city': 'Moscow', 'address':'Lenina12'},
-                   {'name':'Sergey','city': 'Surgut', 'address':'Gagarina42'},
-                   {'name':'Zahar','city': 'Magadan', 'address':'Stalina1'},
-                   {'name':'Dima','city': 'Vologda', 'address':'Centralnay12'},
-                   {'name':'Vera','city': 'Moscow', 'address':'Lenina12'},
-                   {'name':'Vika','city': 'Rostov', 'address':'Tombova45'},
-                   {'name':'Rita','city': 'Nyagan', 'address':'Rovnya41'},
-                   {'name':'Lena','city': 'Kurgan', 'address':'Lenina22'},
-                   {'name':'Slava','city': 'Moscow', 'address':'Stolichnaya12'},
-                   {'name':'Danil','city': 'Surgut', 'address':'Lenina82'}]
+        clients = [{'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)},
+                   {'name':random.choice(a),'city': random.choice(b), 'address':random.choice(c)}]
         CLIENTS.insert_many(clients).execute()
     with db:   
         clients = CLIENTS.select() 
-        orders=[{'client': clients[0],'date':datetime.date(2023, 1, 1),'amount': 10000,'description':'Hmmm.'},
-                {'client': clients[1],'date':datetime.date(2023, 1, 2),'amount': 23333,'description':'Ughm.'},
-                {'client': clients[2],'date':datetime.date(2023, 2, 3),'amount': 14000,'description':'Lol.'},
-                {'client': clients[3],'date':datetime.date(2023, 2, 4),'amount': 1111,'description':':d'},
-                {'client': clients[4],'date':datetime.date(2023, 1, 5),'amount': 102100,'description':':)'},
-                {'client': clients[5],'date':datetime.date(2023, 1, 6),'amount': 12220,'description':'HoHoHo'},
-                {'client': clients[6],'date':datetime.date(2023, 2, 1),'amount': 20000,'description':'Yep'},
-                {'client': clients[7],'date':datetime.date(2023, 1, 10),'amount': 1100,'description':'Ha'},
-                {'client': clients[8],'date':datetime.date(2023, 1, 1),'amount': 800,'description':'H.'},
-                {'client': clients[9],'date':datetime.date(2023, 2, 1),'amount': 324,'description':'Hmmm.'}]
+        orders=[{'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)},
+                {'client': clients[random.randrange(0,9,1)],'date':datetime.date(random.randrange(2020,2023,1),random.randrange(1,12,1), random.randrange(1,12,1)),'amount': random.randrange(1,30,1),'description':random.choice(v)}]
         ORDERS.insert_many(orders).execute()
 def show_c():
     for client in CLIENTS.select():
---
+++ slovar.py
@@ -0,0 +1,4 @@
+a=["Sergey","Andrey","Daniil","Vera","Vika","Rita","Veronika","Ivan","Magomed","Vika" ] #names
+b=["Moscow","Surgut","Chelyabinsc","Irkutsk","Magadan","Kurgan","Barsovo","Solnechnyi","Vladivostok","Krasnoyarsk" ] #city
+c=['Lenina12','Gagarina42','Stalina1','Centralnay12','Lenina12','Tombova45','Rovnya41','Lenina22']#address
+v=['Hmmm.','Ughm.','Lol.',':d',':)','HoHoHo','Yep','Ha']
